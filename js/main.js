0ec87660df0b249a7231892fb8827ae505ba9443
Donay <lucas.donay@gmail.com>
2022-12-01 21:48:35 -0300

    finish project 🚀🚀

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
  var swiper = new Swiper(".slide-characteres", {
-        slidesPerView: 3.5,
+        slidesPerView: 3,
         spaceBetween: 12,
-        freeMode: true
+        freeMode: true,
+        breakpoints: {
+                297: {
+                        slidesPerView: 1.3,
+                        spaceBetween: 33  
+                },
+                500: {
+                        slidesPerView: 2.2,
+                        spaceBetween: 35 
+                 },
+                991: {
+                        slidesPerView: 2.8,
+                        spaceBetween: 35 
+                 },
+                1200: {
+                slidesPerView: 3.2                          
+                }
+              }
+});
+
+AOS.init({
+   duration: 1000,
+   once: true,     
 });
